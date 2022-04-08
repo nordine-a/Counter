c76b4a86896b53eda71780d714702a1d2a367861
Abdessamad Nordine <abdessamadnordine.work@gmail.com>
2022-04-08 15:34:19 +0200

    implemented count

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,26 @@
+
+let screen= document.querySelector("[data-screen]");
+let count= +screen.textContent;
+console.log(count);
 document.addEventListener("click", function(event){
-  
+
+  if(!event.target.dataset.button) return;
+  else if (event.target.dataset.button=="plus") addOneToScreen();
+  else decreaseOneFromScreen();
+
+  changeCount();
 })
+
+
+function addOneToScreen(){
+  count++;
+}
+
+
+function decreaseOneFromScreen(){
+  if(count>0) count--;
+}
+
+function changeCount(){
+  screen.textContent= count;
+}
